23fb9abe5e471d33a5eee33e36c2959038ea031b
TorresMBA <bryan98tm@gmail.com>
2022-11-27 16:10:43 -0500

    Initial Commit

---
+++ Models/Especialidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Models
+{
+    public class Especialidad
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+
+    }
+}
---
+++ Models/TurnosContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Models
+{
+    public class TurnosContext : DbContext
+    {
+        public TurnosContext(DbContextOptions<TurnosContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Especialidad> Especialidad { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad => {
+                entidad.ToTable("Especialidad"); //Nombre de la tabla
+                entidad.HasKey(e => e.Id); //Primary key
+                entidad.Property(e => e.Descripcion).IsRequired().HasMaxLength(200).IsUnicode(false); //agregar nuevo campo y debe se requerido
+            });
+        }
+    }
+}
